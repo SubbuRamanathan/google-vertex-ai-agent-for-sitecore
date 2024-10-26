dda84439cdcfc27e66b634728e633816c7f2eb7e
SubbuRamanathan <sramanathan@subramanian.ca>
2024-10-26 18:50:51 +0000

    Error Handling

--- function-app/index.js
+++ function-app/index.js
@@ -9,16 +9,22 @@ const credentials = require('./key.json');
 const { randomUUID } = require('crypto');
 
 functions.http('post', async (req, res) => {
-    const query = req.body.user_question;
-    const sessionId = req.body.session_id ?? randomUUID();
+    try{
+        const query = req.body.user_question;
+        const sessionId = req.body.session_id ?? randomUUID();
 
-    let infoAgentResponse = await generateResponse(sessionId, query, credentials);
-    let paiInfo = await getPAIInfo(query, infoAgentResponse, credentials);
+        let infoAgentResponse = await generateResponse(sessionId, query, credentials);
+        let paiInfo = await getPAIInfo(query, infoAgentResponse, credentials);
 
-    let modelResponse = await prepareAgentResponse(query, paiInfo, infoAgentResponse, credentials);
-    let finalResponse = {
-        agent_response: modelResponse
-    };
+        let modelResponse = await prepareAgentResponse(query, paiInfo, infoAgentResponse, credentials);
+        let finalResponse = {
+            agent_response: modelResponse
+        };
 
-    res.send(finalResponse);
+        res.send(finalResponse);
+    }
+    catch (error) {
+        console.error("Encountered error while executing the function: ", error);
+        res.send({ agent_response: "Sorry I couldn't find the best response for your question from the supplied information, please send your query to info@company.com" });
+    }
 });
